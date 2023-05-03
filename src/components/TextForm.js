17512b41b462acc71b682be946ecead6be7d9724
faiaz <s.hawks685@gmail.com>
2023-05-03 10:57:52 +0600

    Refactor TextForm

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,45 +1,36 @@
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 import copy from "copy-to-clipboard";
 
 
 export default function TextForm(props){
     const [text,setText] = useState(`Enter text here`);
-    const [ copyText, setCopyText] = useState('')
-    const handleUpClick = ()=>{
-        // console.log("Upper case was clicked" + text);
-        let newText = text.toUpperCase();
-        setText(newText);
-    }
-    const handleLoClick = ()=>{
-        let newText = text.toLowerCase();
-        setText(newText);
-    }
-    const handleClClick = () =>{
-        let newText = " ";
-        setText(newText);
-    }
-    const handleOnChange = (event)=>{
-        // console.log("On Change");
-        setText(event.target.value);
-        setCopyText(event.target.value);
-
-    }
-    const copyOnClick = () =>{
-        copy(copyText);
-        alert(`you have copied "${copyText}"`)
-    }
-
     return(
         <>
         <div className="container">
             <div className="mb-3">
                 <h1>{props.heading}</h1>
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="4"></textarea>
+                <textarea className="form-control" value={text} onChange={(event) => {
+                    // console.log("On Change");
+                    setText(event.target.value);
+                }} id="myBox" rows="4"></textarea>
             </div>
-            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert Uppercase</button>
-            <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert Lowercase</button>
-            <button className="btn btn-primary mx-1" onClick={handleClClick}>Clear Text</button>
-            <button className="btn btn-primary mx-1" onClick={copyOnClick}>Copy Text</button>
+            <button className="btn btn-primary mx-1" onClick={() => {
+                // console.log("Upper case was clicked" + text);
+                let newText = text.toUpperCase();
+                setText(newText);
+            }}>Convert Uppercase</button>
+            <button className="btn btn-primary mx-1" onClick={() => {
+                let newText = text.toLowerCase();
+                setText(newText);
+            }}>Convert Lowercase</button>
+            <button className="btn btn-primary mx-1" onClick={() => {
+                let newText = " ";
+                setText(newText);
+            }}>Clear Text</button>
+            <button className="btn btn-primary mx-1" onClick={() => {
+                copy(text);
+                alert(`you have copied "${text}"`)
+            }}>Copy Text</button>
 
         </div>
             <div className="container my-3">
