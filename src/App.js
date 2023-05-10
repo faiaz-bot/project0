3acb7af7a81a1e254eb7d7e077aacc8bf43f8133
FaiaZ <faiazibneahsan@outlook.com>
2023-05-11 02:13:48 +0600

    refactor dark mode

--- src/App.js
+++ src/App.js
@@ -2,23 +2,25 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import TextForm from "./components/TextForm";
-import {useState} from "react";
+import { useState } from "react";
 
 function App() {
     const [mode, setMode] = useState('light')
     const toggleMode = ()=>{
         if(mode === 'dark'){
             setMode('light')
+            document.body.style.backgroundColor = 'white';
         }
         else {
             setMode('dark')
+            document.body.style.backgroundColor = "#24384c"
         }
     }
   return (
       <>
          <Navbar title="TextUtils" about="About" home="Home" mode={mode} toggleMode={toggleMode}/>
           <div className="container my-3" >
-              <TextForm heading="Enalyze The Text To Analyze"/>
+              <TextForm heading="Enalyze The Text To Analyze" mode={mode}/>
           </div>
           {/*<div className= "container my-5">*/}
           {/*    <About/>*/}
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,13 +3,13 @@ import copy from "copy-to-clipboard";
 
 
 export default function TextForm(props){
-    const [text,setText] = useState(`Enter text here`);
+    const [text,setText] = useState('');
     return(
         <>
         <div className="container">
             <div className="mb-3">
-                <h1>{props.heading}</h1>
-                <textarea className="form-control" value={text} onChange={(event) => {
+                <h1 style={{color: props.mode === 'dark'? 'white' : 'dark' }}>{props.heading}</h1>
+                <textarea className="form-control" style={{background: props.mode === 'dark' ? 'gray' : 'white'  , color: 'white'}} value={text} onChange={(event) => {
                     // console.log("On Change");
                     setText(event.target.value);
                 }} id="myBox" rows="4"></textarea>
@@ -31,17 +31,17 @@ export default function TextForm(props){
                 copy(text);
                 alert(`you have copied "${text}"`)
             }}>Copy Text</button>
-            <button className="btn btn-primary max-1" onClick={() =>{
+            <button className="btn btn-primary mx-1" onClick={() =>{
                 setText(text.replace(/\s+/g, ' ').trim());
             }}>Remove Space</button>
 
         </div>
-            <div className="container my-3">
+            <div className ={`container text-${props.mode === 'dark'?'white': 'dark'} my-3`}>
                 <h1>Your text summery</h1>
                 <p>{text.split(" ").length} words and {text.length} character</p>
                 <p>{0.008 * text.split(" ").length} Minutes read</p>
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text.length > 0 ? text : "Enter Something to textbox to preview it here"}</p>
             </div>
         </>
 )
